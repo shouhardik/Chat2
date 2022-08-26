7759298f986fa09eceef0ce1039e9ea783fa1fee
Shouhardik Saha <60508616+shouhardik@users.noreply.github.com>
2022-08-26 19:23:19 +0530

    Updates

---
+++ precache-manifest.01da3421e5230712036e5bae036af6c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "351c6eb2696d51d4d32d48ee4b9bfade",
+    "url": "/Chat/index.html"
+  },
+  {
+    "revision": "aa81290d08813471148d",
+    "url": "/Chat/static/css/main.4cfdd633.chunk.css"
+  },
+  {
+    "revision": "89c6f0d57413224d7505",
+    "url": "/Chat/static/js/2.8af45b4c.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/Chat/static/js/2.8af45b4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa81290d08813471148d",
+    "url": "/Chat/static/js/main.e346e67b.chunk.js"
+  },
+  {
+    "revision": "c40e6ec3b595af872d83",
+    "url": "/Chat/static/js/runtime-main.a046a700.js"
+  }
+]);
